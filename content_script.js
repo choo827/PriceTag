22f1243e4d4c2cf9dc91ab566076f7fbd3a5d33f
Seungwon Choo <choo827@naver.com>
2020-04-02 01:17:15 +0900

    feat(bubble): bubble을 create/remove하게 만듬

--- content_script.js
+++ content_script.js
@@ -1,62 +1,44 @@
-const bubbleDOM = document.createElement('div');
-const dragCurrency = document.createElement('div');
-const dragPrice = document.createElement('div');
-const exCurrency = document.createElement('div');
-const exPrice = document.createElement('div');
-
-bubbleDOM.setAttribute('id', 'selection_bubble');
-dragCurrency.setAttribute('id', 'drag-currency');
-dragPrice.setAttribute('id', 'drag-price');
-exCurrency.setAttribute('id', 'ex-currency');
-exPrice.setAttribute('id', 'ex-price');
-
-document.body.appendChild(bubbleDOM);
-bubbleDOM.appendChild(dragCurrency);
-bubbleDOM.appendChild(dragPrice);
-bubbleDOM.appendChild(exCurrency);
-bubbleDOM.appendChild(exPrice);
-
+let myCurency;
+chrome.storage.sync.get((item) => {
+    myCurency = item.defaultCurrency;
+});
 
 document.addEventListener('mouseup', (event) => {
     const select = window.getSelection().toString();
     const condition = /(^[£€$￥¥₩￦]\s*[0-9,.]*$)|(^[0-9,.]*[€원]$)/;
-    const filteredSelect = filtering(select, condition);
 
-    if (filteredSelect != '') {
-        const r = window.getSelection().getRangeAt(0).getBoundingClientRect();
-        const relative = document.body.parentNode.getBoundingClientRect();
-        bubbleDOM.style.top = (r.bottom - relative.top) + 'px'; //this will place ele below the selection
-        bubbleDOM.style.left = event.clientX + 'px';
+    if (select != ('' || undefined)) {
+        const filteredSelect = filtering(select, condition);
 
-        let currency;
+        // bubble position
+        // const r = window.getSelection().getRangeAt(0).getBoundingClientRect();
+        // const relative = document.body.parentNode.getBoundingClientRect();
+        // bubbleDOM.style.top = (r.bottom - relative.top) + 'px'; //this will place ele below the selection
+        // bubbleDOM.style.left = event.clientX + 'px';
+
+        let currency, dp, ep;
         if (filteredSelect.charAt(0).match(/[0-9]/g)) {
             // 1000원
             currency = findCurrency(filteredSelect.substr(filteredSelect.length - 1));
-            dragPrice.innerHTML = filteredSelect.slice(0, -1);
-            convertCur(filteredSelect.slice(0, -1)
-                .replace(/,/g, ""), currency);
+            dp = filteredSelect.slice(0, -1);
+            createBubble(currency, dp, myCurency, convertCur(filteredSelect.slice(0, -1)
+                .replace(/,/g, ""), currency));
         } else {
             // $10000
             currency = findCurrency(filteredSelect.charAt(0));
-            dragPrice.innerHTML = filteredSelect.substring(1);
+            dp = filteredSelect.substring(1);
+
             convertCur(filteredSelect.substring(1)
-                .replace(/,/g, ""), currency);
+                .replace(/,/g, ""), currency)
+                .then(data => {
+                    createBubble(currency, dp, myCurency, data.toLocaleString(), currency);
+                });
         }
-        dragCurrency.innerHTML = currency;
-
-        chrome.storage.sync.get((item) => {
-            exCurrency.innerHTML = item.defaultCurrency;
-        });
-
-        bubbleDOM.style.visibility = 'visible';
-        bubbleDOM.style.position = "absolute";
-    } else {
-        bubbleDOM.style.visibility = 'hidden';
     }
 });
 
 document.addEventListener('mousedown', () => {
-    bubbleDOM.style.visibility = 'hidden';
+    removeBubble();
 });
 
 const findCurrency = (rawPrice) => {
@@ -87,17 +69,38 @@ const filtering = (text, condition) => {
 
 const convertCur = (price, currency) => {
     const dataURL = 'https://api.exchangeratesapi.io/latest?base=' + currency;
-    let myBase;
+    return fetch(dataURL).then((res) => {
+        return res.json().then(data => {
+            return data.rates[myCurency] * price;
+        });
+    }).catch(err => console.log(err));
+};
 
-    chrome.storage.sync.get((item) => {
-        myBase = item.defaultCurrency;
-    });
+const createBubble = (_dc, _dp, _ec, _ep) => {
+    const bubbleDOM = document.createElement('div');
+    const dragCurrency = document.createElement('div');
+    const dragPrice = document.createElement('div');
+    const exCurrency = document.createElement('div');
+    const exPrice = document.createElement('div');
 
-    fetch(dataURL)
-        .then((res) => {
-            res.json().then(data => {
-                const convertPrice = data.rates[myBase] * price;
-                exPrice.innerHTML = convertPrice.toLocaleString()
-            })
-        }).catch(err => console.log(err));
+    bubbleDOM.setAttribute('id', 'selection-bubble');
+    dragCurrency.setAttribute('id', 'drag-currency');
+    dragPrice.setAttribute('id', 'drag-price');
+    exCurrency.setAttribute('id', 'ex-currency');
+    exPrice.setAttribute('id', 'ex-price');
+
+    document.body.appendChild(bubbleDOM);
+    bubbleDOM.appendChild(dragCurrency);
+    bubbleDOM.appendChild(dragPrice);
+    bubbleDOM.appendChild(exCurrency);
+    bubbleDOM.appendChild(exPrice);
+
+    dragCurrency.innerHTML = _dc;
+    dragPrice.innerHTML = _dp;
+    exCurrency.innerHTML = _ec;
+    exPrice.innerHTML = _ep;
+};
+
+const removeBubble = () => {
+    document.getElementById('selection-bubble').remove();
 };
